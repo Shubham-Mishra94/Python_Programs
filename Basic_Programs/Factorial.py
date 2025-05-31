78ba05ec80dcf8c71993276c797f6ea54c0c4bf3
Shubham <171994sm@gmail.com>
2025-05-31 23:21:28 +0530

    Add files via upload

---
+++ Basic_Programs/Factorial.py
@@ -0,0 +1,13 @@
+# Finding factorial
+
+import math
+
+def factorial(n):
+    return(math.factorial(n))
+
+
+
+
+num = int(input("Enter the Number: "))
+print("Fcatorial of ", num, "is", factorial(num))
+
